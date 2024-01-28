067c006ebb305e77776f3708d2a85b67e0e58adc
Pavitra674 <108884384+Pavitra674@users.noreply.github.com>
2024-01-28 12:59:01 +0530

    Delete dbConnect.js

--- dbConnect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheymoney-udemy' , {useNewUrlParser : true , useUnifiedTopology : true})
-
-const connection = mongoose.connection
-
-connection.on('error', err => console.log(err))
-
-connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
